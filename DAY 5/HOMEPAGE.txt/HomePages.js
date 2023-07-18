dad38dab8f854aefb3079e116004adf529fd8469
harishragavedar <106361974+harishragavedar@users.noreply.github.com>
2023-07-18 15:39:09 +0530

    Add files via upload

---
+++ DAY 5/HOMEPAGE.txt/HomePages.js
@@ -0,0 +1,15 @@
+import Home from "../home/homes/Home";
+import React from "react";
+import Streams from "../home/homes/Streams";
+
+
+const HomePages =()=>
+{
+    return(
+        <>
+        <Home/>
+        <Streams/>
+        </>
+    )
+}
+export default HomePages;
