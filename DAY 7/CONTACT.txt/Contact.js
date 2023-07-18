5ebbc7d3f1f0fb9f5eef0a71214493615f332a12
harishragavedar <106361974+harishragavedar@users.noreply.github.com>
2023-07-18 15:40:11 +0530

    Add files via upload

---
+++ DAY 7/CONTACT.txt/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Contact.css"
+
+const Contact = ()=>
+{
+    return(
+        <>
+        <section className={styles.container}>
+        <div className="contact"></div>
+        <div className={styles.contact_form}></div>
+        <div className={styles.contact_image}></div>
+            </section>    
+        </>
+    )
+}
